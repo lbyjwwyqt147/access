fb2c49a68cf81c36ab879933ff038e84c1439319
lbyjwwyqt147 <ljy671018@163.com>
2017-07-18 23:02:23 +0800

    注册

---
+++ resources/scripts/role/role_add.js
@@ -0,0 +1,37 @@
+var RoleForm  = function () {
+    var basicUrl = commonUtil.httpUrl;
+
+    function submitForm() {
+        $.ajax({
+            url: basicUrl+ "/roles",
+            data:$("#addRoleForm").serialize(),
+            type:"POST",
+            dataType:"json",
+            success :function (data,textStatus) {
+                console.log(data);
+            },
+            error:function (XMLHttpRequest, textStatus, errorThrown) {
+
+            }
+        });
+    }
+
+
+    $('#save-button').on('click', function(){
+        submitForm();
+    });
+
+
+    return {
+        //main function to initiate the module
+        init: function () {
+
+
+
+        }
+    };
+}();
+
+jQuery(document).ready(function() {
+    RoleForm.init();
+});
